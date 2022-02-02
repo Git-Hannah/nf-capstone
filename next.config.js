db6e7b38f3cd957b41053cb3edcbfe7f54fece69
Hannah <hannahsenft@gmx.net>
2022-02-02 15:05:39 +0100

    feat(Image): add Hero Image

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@
 const nextConfig = {
 	/* config options here */
 	reactStrictMode: true,
+	images: {
+		domains: ["images.unsplash.com"],
+	},
 };
 
 module.exports = nextConfig;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,38 @@
 import React from "react";
 import Image from "next/image";
 import Button from "../atoms/button";
+import styled from "@emotion/styled";
 
 // image link:
 // https://images.unsplash.com/photo-1514306191717-452ec28c7814?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80
 
-const Page = () => {
-
+const StyledImage = styled(Image)`
+	border-radius: 100px;
+`;
+const A = ({ children, className }) => {
 	return (
-		<div>
-			<Image 
-				src="/"
+		<div className={className}>
+			{children}
+		</div>
+	)
+}
+const B = styled(A)`
+background: pink;
+`
+
+const Page = () => {
+	return (
+		<B>
+			<StyledImage
+				src="https://images.unsplash.com/photo-1514306191717-452ec28c7814?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"
+				//src="/../public/red-curtain.jpg"
 				alt="This is a placeholder for an awesome image"
-				width={500}
+				width={800}
 				height={500}
+				layout="fixed"
 			/>
 			<Button>Mime Workout</Button>
-
-		</div>
+		</B>
 	);
 };
 

f8956064c4e0f89c1dad5395e36c2e36b30a47c5
Hannah <hannahsenft@gmx.net>
2022-02-02 10:49:24 +0100

    chore: install mui/material emotion/react emotion/styled

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
+// image link:
+// https://images.unsplash.com/photo-1514306191717-452ec28c7814?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80
 
 const Page = () => {
 
 	return (
 		<div>
-			This is my page
+			<Image 
+				src="/"
+				alt="This is a placeholder"
+				width={500}
+				height={500}
+			/>
+			
+
 		</div>
 	);
 };

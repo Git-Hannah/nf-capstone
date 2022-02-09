6b9376fed227246e4bce01f2281d41afd5715094
Hannah <hannahsenft@gmx.net>
2022-02-09 09:48:19 +0100

    feat(images): fix paths to images in exercises.jsx

--- src/ions/exercises.jsx
+++ src/ions/exercises.jsx
@@ -5,7 +5,7 @@ export const exercises = [
 		description:
 			"Put your hand on a fixed object like a table or wall, move around it with your body without moving the hand. Notice how your arm moves and looks. Take your hand an inch away from the object, let it hover there and repeat the movements, again without moving the hand. Try with different states of muscle tension!",
 		duration: "10min", // 10 minutes -> 10*60*1000
-		image: "/../public/images/fixpoint.png",
+		image: "/images/fixpoint.png",
 	},
 	{
 		id: "Imaginary Object",
@@ -13,7 +13,7 @@ export const exercises = [
 		description:
 			"Take a real object, e.g. a glass. Pick it up, and feel the form, weight and temperature. Put it down, and pick up an imaginary glass. Form the glass with your hand with the attributes from your memory!",
 		duration: 180000, // 3min
-		image: "/../public/images/imaginary-object.png",
+		image: "/images/imaginary-object.png",
 	},
 	{
 		id: "Walking on the spot",
@@ -21,7 +21,7 @@ export const exercises = [
 		description:
 			"1. Walk backwards, starting from the tip of your foot, use the full range of movement 2. Everytime you put your toe on the ground, hold your balance here for a moment, the other foot in the air 3. The foot that is going forward while walking backwards can now be redirected to go backwards, while keeping the exact same sequence of movement 4. Now everytime your toe comes to the ground, place it on the same spot every time 5. Mime walk on the spot!",
 		duration: 600000, // 10min
-		image: "/../public/images/walking-on-the-spot.png",
+		image: "/images/walking-on-the-spot.png",
 	},
 	{
 		id: "Clown",
@@ -29,6 +29,6 @@ export const exercises = [
 		description:
 			"Scratch your nose to feel it, then imagine your nose has an eye which wants to see everything first. Explore your surroundings, leading with your nose!",
 		duration: "5min", // 5 minutes -> 5*60*1000
-		image: "/../public/images/clown.png",
+		image: "/images/clown.png",
 	},
 ];

cf591e8aa1e0ef1fb4867c58741a4c2db5bd2b82
Hannah <hannahsenft@gmx.net>
2022-02-17 16:09:23 +0100

    feat(favorite): add favorite buttons from mui

--- src/molecules/card-exercise/index.jsx
+++ src/molecules/card-exercise/index.jsx
@@ -7,6 +7,9 @@ import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
 import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import BubbleChartIcon from '@mui/icons-material/BubbleChart';
+import StarIcon from '@mui/icons-material/Star';
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Image from "next/image";
@@ -32,6 +35,9 @@ const CardExercise = ({ name, description, duration, image }) => {
 
 	return (
 		<Card sx={{ maxWidth: 345 }}>
+			<IconButton aria-label="add to favorites">
+				<StarIcon />
+			</IconButton>
 			<CardHeader title={name} />
 			<Image src={image} height="50vw" width="80vw" alt="Fixpoint" />
 			<CardContent>Time: {duration}</CardContent>

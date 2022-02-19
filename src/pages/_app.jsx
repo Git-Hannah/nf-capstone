dc607f35bdf85f8189fe86d8fc2f0b64d107e24f
Hannah <hannahsenft@gmx.net>
2022-02-19 22:05:31 +0100

    feat(login): add login with github and install next-auth

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,16 @@
 import { Global } from "@emotion/react";
 import React from "react";
 import { globalStyle } from "../ions/styles";
+import { SessionProvider } from "next-auth/react"
+
 
 const App = ({ Component, pageProps }) => {
 	return (
-		<>
+		<SessionProvider session={pageProps.session}>
+	
 			<Global styles={globalStyle} />
 			<Component {...pageProps} />
-		</>
+		</SessionProvider>
 	);
 };
 

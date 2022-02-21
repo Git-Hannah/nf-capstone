d02eadbdadc016b8b3814cce1e6c482d2625dae4
Hannah <hannahsenft@gmx.net>
2022-02-21 11:31:53 +0100

    feat(admin): fix admin login with dotenv and nexconfig

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+require("dotenv").config;
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
 	/* config options here */
 	reactStrictMode: true,
+	env: {
+		ADMIN_USER_KEY: process.env.ADMIN_USER_KEY,
+	},
 	images: {
 		domains: ["images.unsplash.com", "http.cat"],
 	},

e847991d445660fbb2b0bde340aea4c97d1b83aa
Git-Hannah <72019322+Git-Hannah@users.noreply.github.com>
2022-02-21 11:07:49 +0100

    Feature/styling (#22)
    * feat(login): add login with github and install next-auth
    
    * feat(login): install @next-auth/mongodb-adapter and mongodb
    
    * feat(login): starting on restricted post of exercises
    
    * feat(404): add http.cat/404 to 404.jsx just in case
    
    * feat(navbar): add navbar component
    
    * feat(admin): add protected routes

--- src/pages/api/exercises.js
+++ src/pages/api/exercises.js
@@ -1,10 +1,11 @@
 import dbConnect from "../../server/mongoose";
 import Exercise from "../../server/models/exercise";
+import { getSession } from "next-auth/react";
 
 export default async function handler(request, response) {
 	const { method, body } = request;
+	const session = await getSession({ req: request });
 	await dbConnect();
-
 
 	switch (method) {
 		case "GET":
@@ -17,12 +18,19 @@ export default async function handler(request, response) {
 			}
 			break;
 		case "POST":
-			try {
-				const exercise = await Exercise.create(body); /* create a new model in the database */
-				response.status(201).json(exercise);
-			} catch (error) {
-				response.status(400).json({ success: false });
+			if (session && session.user.id === process.env.ADMIN_USER_KEY) {
+				try {
+					const exercise = await Exercise.create(
+						body
+					); /* create a new model in the database */
+					response.status(201).json(exercise);
+				} catch (error) {
+					response.status(400).json({ success: false });
+				}
+			} else {
+				response.status(401).json({ message: "Unauthorized" });
 			}
+
 			break;
 		default:
 			response.status(400).json({ success: false });

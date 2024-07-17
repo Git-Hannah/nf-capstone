ad86399c75eb778fbf7825e580a08f7015d2b870
Hannah <72019322+Git-Hannah@users.noreply.github.com>
2024-07-17 16:57:46 +0200

    Thumbnail (#33)
    * add icon image files
    
    * start thumbnail adding process
    
    * feat: add metadata
    
    * fix: comment
    
    * fix: image comment position
    
    * fix: image comment eslint
    
    * fix: image comment eslint
    
    * fix: image comment eslint
    
    * feat: change favicon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Head from "next/head";
 import Image from "next/image";
 import styled from "@emotion/styled";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Link from "next/link";
 
+// const Metadata
+//src="https://images.unsplash.com/photo-1542887800-faca0261c9e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=727&q=80"
 // This is the style for the image
 const StyledImage = styled(Image)`
 	object-fit: cover;
@@ -24,9 +27,21 @@ const StyledWholePage = styled(WholePage)`
 const Page = () => {
 	return (
 		<StyledWholePage>
+			<Head>
+				<title key="title">MiME workout app</title>
+				<meta
+					key="description"
+					name="description"
+					content="Simple workouts for Mimes of any level"
+				/>
+				<meta property="og:image" content="./public/images/mime-icon.png" />
+				<meta property="og:image:width" content="1200" />
+				<meta property="og:image:height" content="627" />
+				<meta property="og:type" content="application" />
+			</Head>
+
 			<StyledImage
 				src="/images/hands.jpg"
-				//src="https://images.unsplash.com/photo-1542887800-faca0261c9e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=727&q=80"
 				alt="Expressive Hands in a spotlight in black and white"
 				layout="fill"
 			/>
@@ -42,6 +57,8 @@ const Page = () => {
 					px: 3,
 				}}
 			>
+				{" "}
+				{/* The white button that links to the workout overview */}
 				<Link passHref href="/workout-overview">
 					<Button
 						variant="contained"

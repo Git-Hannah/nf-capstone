49f097fe9970a150a200a2c04ab27d8588c41501
Hannah <72019322+Git-Hannah@users.noreply.github.com>
2024-07-17 16:29:48 +0200

    fix: image comment eslint

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -78,4 +78,6 @@ const Page = () => {
 };
 
 export default Page;
-// original image source: src="https://images.unsplash.com/photo-1542887800-faca0261c9e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=727&q=80"
+{
+	/* original image source: src="https://images.unsplash.com/photo-1542887800-faca0261c9e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=727&q=80" */
+}
